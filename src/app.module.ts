b2b0b73e1b702a7db76cf80431e149d6b755673f
charmingruby <mrrubycanal3105@gmail.com>
2024-10-30 10:41:38 -0300

    feat: creates typeorm entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ContentController } from './http/rest/controller/content.controller';
 import { ContentManagementService } from './core/service/content-management.service';
 import { MediaPlayerService } from './core/service/media-player.service';
-import { PrismaService } from './persistence/prisma/prisma.service';
 import { ContentRepository } from './persistence/repository/content.repository';
 import { VideoRepository } from './persistence/repository/video.repository';
 import { MediaPlayerController } from './http/rest/controller/media-player.controller';
@@ -12,7 +11,6 @@ import { ConfigModule } from './infra/module/config/config.module';
   imports: [ConfigModule.forRoot()],
   controllers: [ContentController, MediaPlayerController],
   providers: [
-    PrismaService,
     ContentManagementService,
     MediaPlayerService,
     ContentRepository,
